08c1b2d7f48a336590c6543355d9ba2b9f9597ff
Daniel E. Sejersen <Daniel.sejersen@hotmail.com>
2021-11-18 13:53:59 +0100

    First broken version of trash parameter

--- dijkstra.c
+++ dijkstra.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 #define TRUE 1
 #define FALSE 0
 #define NUMBEROFNODE 10
@@ -9,11 +8,13 @@ int findShortestUnvisited(int visited[NUMBEROFNODE], int shortestDistance[NUMBER
 void findShortestPath(int graph[NUMBEROFNODE][NUMBEROFNODE]);
 void findLenghtsFromNode(int currentNode, int graph[NUMBEROFNODE][NUMBEROFNODE], int shortestDistance[NUMBEROFNODE], int nodeParent[NUMBEROFNODE]);
 void printResult(int nodeParent[NUMBEROFNODE], int shortestDistance[NUMBEROFNODE], int visited[NUMBEROFNODE]);
+int trashRoutePlanner(int visited[NUMBEROFNODE], int shortestDistance[NUMBEROFNODE], int nodeParent[NUMBEROFNODE], int trashCompactness[NUMBEROFNODE], int finalRoute[20], int currentNode, int routeNum);
 
 int main(void)
 {
 
     int graph[NUMBEROFNODE][NUMBEROFNODE] = {
+        /*     0  1  2  3  4  5  6  7  8  9*/
         /*0*/ {0, 1, 0, 2, 0, 0, 0, 0, 0, 0},
         /*1*/ {1, 0, 3, 4, 1, 0, 0, 0, 0, 0},
         /*2*/ {0, 3, 0, 3, 0, 0, 0, 0, 0, 0},
@@ -31,15 +32,27 @@ int main(void)
 
 void findShortestPath(int graph[NUMBEROFNODE][NUMBEROFNODE])
 {
-    //Node: 0-1-2-3-4
 
     //they are all false since they have not been visited.
-    int visited[NUMBEROFNODE] = {FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE};
+    int visited[NUMBEROFNODE] = {FALSE};
 
     //unviseted nodes have a distance of infinity since we dont know the distance. start node has a distance of 0 since that's the distance to itself.
-    int shortestDistance[NUMBEROFNODE] = {0, __INT_MAX__, __INT_MAX__, __INT_MAX__, __INT_MAX__, __INT_MAX__, __INT_MAX__, __INT_MAX__, __INT_MAX__, __INT_MAX__};
+    int shortestDistance[NUMBEROFNODE];
+
+    for (int i = 0; i < NUMBEROFNODE; i++)
+    {
+        shortestDistance[i] = __INT_MAX__;
+    }
+
     //nodeParent defines the shortes path to a node from previous node. since no parent has been found yet all a -1
-    int nodeParent[NUMBEROFNODE] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int nodeParent[NUMBEROFNODE] = {0};
+
+    int trashCompactness[NUMBEROFNODE] = {0, 69, 0, 75, 70, 71, 0, 100, 71, 0};
+
+    int finalRoute[20] = {0};
+
+    shortestDistance[0] = 0;
+    trashCompactness[0] = 0;
 
     int currentNode = 0;
 
@@ -48,11 +61,34 @@ void findShortestPath(int graph[NUMBEROFNODE][NUMBEROFNODE])
         currentNode = findShortestUnvisited(visited, shortestDistance, nodeParent, currentNode);
 
         visited[currentNode] = TRUE;
+        trashCompactness[currentNode] = 0;
 
         findLenghtsFromNode(currentNode, graph, shortestDistance, nodeParent);
     }
 
     printResult(nodeParent, shortestDistance, visited);
+
+    trashRoutePlanner(visited, shortestDistance, nodeParent, trashCompactness, finalRoute, 0, 0);
+    printf("\n");
+    for (int i = 0; i < NUMBEROFNODE; i++)
+    {
+        printf("%d\t", finalRoute[i]);
+    }
+    printf("\n");
+}
+
+int trashRoutePlanner(int visited[NUMBEROFNODE], int shortestDistance[NUMBEROFNODE], int nodeParent[NUMBEROFNODE], int trashCompactness[NUMBEROFNODE], int finalRoute[20], int currentNode, int routeNum)
+{
+    int compareNode = __INT_MAX__;
+    int flag = FALSE;
+  
+    
+
+    if (routeNum != NUMBEROFNODE)
+    {
+        trashRoutePlanner(visited, shortestDistance, nodeParent, trashCompactness, finalRoute, currentNode, routeNum + 1);
+        return currentNode;
+    }
 }
 
 void findLenghtsFromNode(int currentNode, int graph[NUMBEROFNODE][NUMBEROFNODE], int shortestDistance[NUMBEROFNODE], int nodeParent[NUMBEROFNODE])
@@ -62,13 +98,13 @@ void findLenghtsFromNode(int currentNode, int graph[NUMBEROFNODE][NUMBEROFNODE],
     {
         if (graph[currentNode][i] != 0 && shortestDistance[currentNode] + graph[currentNode][i] < shortestDistance[i])
         {
-            //the new distance is equal to the weight up to current node plus the new. 
+            //the new distance is equal to the weight up to current node plus the new.
             shortestDistance[i] = shortestDistance[currentNode] + graph[currentNode][i];
             nodeParent[i] = currentNode;
         }
     }
 }
-
+//
 int findShortestUnvisited(int visited[NUMBEROFNODE], int shortestDistance[NUMBEROFNODE], int nodeParent[NUMBEROFNODE], int currentNode)
 {
     int minimum = __INT_MAX__;
